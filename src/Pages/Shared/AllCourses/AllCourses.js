9bbe1004d10ca0653d7a09fed89ad6268c29ca04
mamun-prodhan <mamunpr9@gmail.com>
2022-10-31 16:24:37 +0600

    home page design completed

--- src/Pages/Shared/AllCourses/AllCourses.js
+++ src/Pages/Shared/AllCourses/AllCourses.js
@@ -5,17 +5,19 @@ import Row from 'react-bootstrap/Row';
 import Button from 'react-bootstrap/Button';
 
 const AllCourses = ({ course }) => {
+    console.log(course);
     return (
         <Col>
           <Card>
-            <Card.Img variant="top" src="holder.js/100px160" />
+            <Card.Img variant="top" src={course.image_url} />
             <Card.Body>
-              <Card.Title>Card title</Card.Title>
+              <Card.Title>{course.title}</Card.Title>
               <Card.Text>
                 This is a longer card with supporting text below as a natural
                 lead-in to additional content. This content is a little bit
                 longer.
               </Card.Text>
+              <Button variant="outline-primary">Primary</Button>
             </Card.Body>
           </Card>
         </Col>
